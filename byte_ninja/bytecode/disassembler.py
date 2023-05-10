272c2cd3077d93c302687eb0d5febe6525bed0c8
Cjw9000-py <cjw@thedeadfly.com>
2023-05-10 22:42:36 +0200

    added a disassembler

---
+++ byte_ninja/bytecode/disassembler.py
@@ -0,0 +1,159 @@
+from .token import *
+from .codes import *
+from .sizes import *
+from .code_object import CodeObject
+
+from io import StringIO
+from enum import IntEnum
+
+
+class Disassembler:
+    class PatternType(IntEnum):
+        EMIT_LEVEL = 0
+        EMIT_ENUM = 1
+        EMIT_NAME = 2
+        EMIT_INTEGER = 3
+        EMIT_TOKEN = 4
+
+    TOK_EOL = -1
+
+    def __init__(self, code: CodeObject):
+        self.code = code
+        self.source = StringIO()
+        self.indent_level = 0
+        self.line_buffer = StringIO()
+
+        emit_level = (self.PatternType.EMIT_LEVEL,)
+        emit_name = (self.PatternType.EMIT_NAME,)
+
+        emit_integer = lambda s: (self.PatternType.EMIT_INTEGER, s)
+        emit_enum = lambda opts, s: (self.PatternType.EMIT_ENUM, opts, s)
+        emit_token = lambda t: (self.PatternType.EMIT_TOKEN, t)
+        emit_eol = (self.PatternType.EMIT_TOKEN, self.TOK_EOL)
+
+        self.patterns = {
+            OPCode.GET: [emit_name, emit_eol],
+            OPCode.PUT: [emit_name, emit_eol],
+            OPCode.EMPTY: [emit_integer(WORD), emit_eol],
+            OPCode.EDIT: [emit_integer(WORD), emit_eol],
+            OPCode.FLIP: [emit_integer(WORD), emit_eol],
+            OPCode.YIELD: [emit_integer(WORD), emit_eol],
+            OPCode.APPEND: [emit_integer(WORD), emit_eol],
+            OPCode.INDEX: [emit_integer(WORD), emit_eol],
+            OPCode.FINISH: [emit_integer(WORD), emit_eol],
+            OPCode.FORGET: [emit_integer(WORD), emit_eol],
+            OPCode.PUSH: [emit_integer(QWORD), emit_eol],
+            OPCode.POP: [emit_eol],
+            OPCode.READ: [emit_name, emit_eol],
+            OPCode.RARRAY: [emit_name, emit_eol],
+            OPCode.WRITE: [emit_name, emit_eol],
+            OPCode.WARRAY: [emit_name, emit_eol],
+            OPCode.LOOP: [emit_token(TOK_COLON), emit_eol, emit_level, emit_eol],
+            OPCode.LOOPX: [emit_token(TOK_COLON), emit_eol, emit_level, emit_eol],
+            OPCode.BREAK: [emit_eol],
+            OPCode.TEST: [emit_enum({
+                TestOperation.EQ: TOK_EQ,
+                TestOperation.NE: TOK_NE,
+                TestOperation.LT: TOK_LT,
+                TestOperation.GT: TOK_GT,
+                TestOperation.LE: TOK_LE,
+                TestOperation.GE: TOK_GE,
+                TestOperation.NOT: TOK_NOT,
+            }, BYTE), emit_token(TOK_COLON), emit_eol, emit_level, emit_eol],
+            OPCode.RET: [emit_eol],
+            OPCode.SEEK: [emit_enum({
+                SeekMode.START: TOK_SEEK_START,
+                SeekMode.REL: TOK_SEEK_REL,
+                SeekMode.END: TOK_SEEK_END,
+            }, BYTE), emit_eol],
+            OPCode.TELL: [emit_eol],
+            OPCode.ENCODE: [emit_name, emit_eol],
+            OPCode.DECODE: [emit_name, emit_eol],
+        }
+
+        while not self.code.eof:
+            self.emit_instruction()
+
+    def emit(self, source: str):
+        indent = ' ' * 4
+
+        while source:
+            # read a line segment
+            line_seg, eol, source = source.partition('\n')
+
+            self.line_buffer.write(line_seg)
+
+            if not eol:
+                break
+
+            # we reached eol, flush the buffer
+            self.source.write(
+                indent * self.indent_level + self.line_buffer.getvalue() + eol
+            )
+
+            self.line_buffer.seek(0)
+            self.line_buffer.truncate()
+
+
+    def emit_integer(self, size: int):
+        value = self.code.read_uint(size)
+        self.emit(str(value))
+
+    def emit_enum(self, opts: dict[int, TokenType], size: int):
+        value = self.code.read_uint(size)
+
+        tok = TOKEN_TYPE_TO_EXACT[opts[value]]
+        self.emit(tok)
+
+    def emit_name(self):
+        self.emit(
+            self.code.read_name()
+        )
+
+    def emit_token(self, typ: TokenType):
+        if typ == -1:
+            self.emit('\n')
+            return
+
+        self.emit(TOKEN_TYPE_TO_EXACT[typ])
+
+    def emit_pattern(self, pattern: list[tuple]):
+
+        for sub in pattern:
+            if sub[0] == self.PatternType.EMIT_LEVEL:
+                self.emit_level()
+            elif sub[0] == self.PatternType.EMIT_NAME:
+                self.emit_name()
+            elif sub[0] == self.PatternType.EMIT_ENUM:
+                self.emit_enum(*sub[1:])
+            elif sub[0] == self.PatternType.EMIT_INTEGER:
+                self.emit_integer(*sub[1:])
+            elif sub[0] == self.PatternType.EMIT_TOKEN:
+                self.emit_token(*sub[1:])
+            else:
+                assert False
+
+    def emit_instruction(self):
+        op = self.code.read_uint(BYTE)
+
+        self.emit(
+            TOKEN_TYPE_TO_EXACT[
+                OPCODE_TO_TOKEN[op]
+            ]
+        )
+
+        self.emit_pattern(
+            self.patterns[op]
+        )
+
+    def emit_level(self):
+        self.indent_level += 1
+
+        # read count
+        count = self.code.read_uint(QWORD)
+
+        for _ in range(count):
+            self.emit_instruction()
+
+        self.indent_level -= 1
+        self.emit_token(TOK_END)
---
+++ tests/test_bytecode/test_disassembler.py
@@ -0,0 +1,53 @@
+from byte_ninja.bytecode.assembler import *
+from byte_ninja.bytecode.disassembler import *
+from byte_ninja.bytecode.code_object import CodeObject
+
+def strip(source: str) -> str:
+    return (
+        source
+        .replace(' ', '')
+        .replace('\t', '')
+        .replace('\n', '')
+        .replace('\r', '')
+    )
+
+def run(source: str):
+    asm = Assembler(source, OperationMode.READ)
+
+    obj = CodeObject.from_bytes(
+        asm.get_output()
+    )
+
+    dis = Disassembler(obj)
+    dis_source = dis.source.getvalue()
+
+    assert strip(source) == strip(dis_source)
+
+
+
+def test_get(): run('get name')
+def test_put(): run('put name')
+def test_empty(): run('empty 0')
+def test_edit(): run('edit 0')
+def test_flip(): run('flip 0')
+def test_yield(): run('yield 0')
+def test_append(): run('append 0')
+def test_index(): run('index 0')
+def test_finish(): run('finish 0')
+def test_forget(): run('forget 0')
+def test_push(): run('push 0')
+def test_pop(): run('pop')
+def test_read(): run('read name')
+def test_rarray(): run('rarray name')
+def test_write(): run('write name')
+def test_warray(): run('warray name')
+def test_loopx(): run('loopx: pop end')
+def test_loop(): run('loop: pop end')
+def test_break(): run('break')
+def test_test(): run('test EQ: pop end')
+def test_ret(): run('ret')
+def test_seek(): run('seek END')
+def test_tell(): run('tell')
+def test_encode(): run('encode name')
+def test_decode(): run('decode name')
+
